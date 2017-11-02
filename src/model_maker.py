fdb9098454d9af07369821658470d683a1b8541c
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2017-11-02 19:35:50 +0530

    refactored code

--- src/model_maker.py
+++ src/model_maker.py
@@ -9,59 +9,11 @@ Aditya Grover and Jure Leskovec
 Knowledge Discovery and Data Mining (KDD), 2016
 '''
 
-import argparse
 import numpy as np
 import networkx as nx
 import node2vec
 from gensim.models import Word2Vec
 
-def parse_args():
-	'''
-	Parses the node2vec arguments.
-	'''
-	parser = argparse.ArgumentParser(description="Run node2vec.")
-
-	parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
-	                    help='Input graph path')
-
-	parser.add_argument('--output', nargs='?', default='emb/karate.emb',
-	                    help='Embeddings path')
-
-	parser.add_argument('--dimensions', type=int, default=128,
-	                    help='Number of dimensions. Default is 128.')
-
-	parser.add_argument('--walk-length', type=int, default=80,
-	                    help='Length of walk per source. Default is 80.')
-
-	parser.add_argument('--num-walks', type=int, default=10,
-	                    help='Number of walks per source. Default is 10.')
-
-	parser.add_argument('--window-size', type=int, default=10,
-                    	help='Context size for optimization. Default is 10.')
-
-	parser.add_argument('--iter', default=1, type=int,
-                      help='Number of epochs in SGD')
-
-	parser.add_argument('--workers', type=int, default=8,
-	                    help='Number of parallel workers. Default is 8.')
-
-	parser.add_argument('--p', type=float, default=1,
-	                    help='Return hyperparameter. Default is 1.')
-
-	parser.add_argument('--q', type=float, default=1,
-	                    help='Inout hyperparameter. Default is 1.')
-
-	parser.add_argument('--weighted', dest='weighted', action='store_true',
-	                    help='Boolean specifying (un)weighted. Default is unweighted.')
-	parser.add_argument('--unweighted', dest='unweighted', action='store_false')
-	parser.set_defaults(weighted=False)
-
-	parser.add_argument('--directed', dest='directed', action='store_true',
-	                    help='Graph is (un)directed. Default is undirected.')
-	parser.add_argument('--undirected', dest='undirected', action='store_false')
-	parser.set_defaults(directed=False)
-
-	return parser.parse_args()
 
 def read_graph(args):
 	'''
@@ -104,20 +56,3 @@ def model_maker(args):
 	walks = G.simulate_walks(args.num_walks, args.walk_length)
 	model = learn_embeddings(walks, args)
 	return model
-
-#nodes = np.load('/home/master/Desktop/GIT/node2vec-final/node2vec/src/nodes.npy')
-def word2id(word):
-	
-	for node in nodes:
-		if node[1]["label"] == word:
-			return str(node[0])
-	raise "word not in vocab"
-
-if __name__ == "__main__":
-	args = parse_args()
-	#model = main(args)
-	
-	#w1 = "you"
-	#w2 = "yourself"
-	#print(model.wv.similarity(word2id(w1), word2id(w2)))
-	#print(model.wv.similarity("1", "3"))
