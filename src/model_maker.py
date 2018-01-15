af3904a6655a5a3493cf455ccc1a8c57e0942e57
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2018-01-15 16:52:46 +0530

    removed unnecessary files

--- src/model_maker.py
+++ src/model_maker.py
@@ -15,60 +15,15 @@ import networkx as nx
 import node2vec
 from gensim.models import Word2Vec
 
-def parse_args():
-	'''
-	Parses the node2vec arguments.
-	'''
-	parser = argparse.ArgumentParser(description="Run node2vec.")
-
-	parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
-	                    help='Input graph path')
-
-	parser.add_argument('--output', nargs='?', default='emb/karate.emb',
-	                    help='Embeddings path')
-
-	parser.add_argument('--dimensions', type=int, default=128,
-	                    help='Number of dimensions. Default is 128.')
-
-	parser.add_argument('--walk-length', type=int, default=80,
-	                    help='Length of walk per source. Default is 80.')
-
-	parser.add_argument('--num-walks', type=int, default=10,
-	                    help='Number of walks per source. Default is 10.')
-
-	parser.add_argument('--window-size', type=int, default=10,
-                    	help='Context size for optimization. Default is 10.')
-
-	parser.add_argument('--iter', default=1, type=int,
-                      help='Number of epochs in SGD')
-
-	parser.add_argument('--workers', type=int, default=8,
-	                    help='Number of parallel workers. Default is 8.')
-
-	parser.add_argument('--p', type=float, default=1,
-	                    help='Return hyperparameter. Default is 1.')
-
-	parser.add_argument('--q', type=float, default=1,
-	                    help='Inout hyperparameter. Default is 1.')
-
-	parser.add_argument('--weighted', dest='weighted', action='store_true',
-	                    help='Boolean specifying (un)weighted. Default is unweighted.')
-	parser.add_argument('--unweighted', dest='unweighted', action='store_false')
-	parser.set_defaults(weighted=False)
-
-	parser.add_argument('--directed', dest='directed', action='store_true',
-	                    help='Graph is (un)directed. Default is undirected.')
-	parser.add_argument('--undirected', dest='undirected', action='store_false')
-	parser.set_defaults(directed=False)
-
-	return parser.parse_args()
-
 def read_graph(args):
 	'''
 	Reads the input network in networkx.
 	'''
 	if args.graph:
-		G = nx.read_edgelist(nx.generate_edgelist(args.graph, data=True), nodetype=int, create_using=nx.DiGraph())
+		G1 = nx.read_edgelist(nx.generate_edgelist(args.graph, data=True), nodetype=int, create_using=nx.DiGraph())
+		G = args.graph
+		#print G.nodes(), G1.nodes()
+		#print G.edges(), G1.edges()
 		for edge in G.edges():
 			G[edge[0]][edge[1]]['weight'] = 1
 		return G
@@ -84,40 +39,27 @@ def read_graph(args):
 
 	return G
 
-def learn_embeddings(walks, args):
+def learn_embeddings(walks, args, unique_words):
 	'''
 	Learn embeddings by optimizing the Skipgram objective using SGD.
 	'''
-	walks = [map(str, walk) for walk in walks] 
+	#walks = [map(str, walk) for walk in walks] 
+	walks = [[unique_words[item] for item in walk] for walk in walks]	
+	# TODO change here
+	# instead of converting ids to string pass their corresponding words
+	
 	model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers, iter=args.iter)
 	#model.wv.save_word2vec_format(args.output)
 	
 	return model
 
-def model_maker(args):
+def model_maker(args, unique_words):
 	'''
 	Pipeline for representational learning for all nodes in a graph.
 	'''
 	nx_G = read_graph(args)
 	G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
 	G.preprocess_transition_probs()
-	walks = G.simulate_walks(args.num_walks, args.walk_length)
-	model = learn_embeddings(walks, args)
+	walks = G.simulate_walks(args.num_walks, args.walk_length, args.meta_paths)
+	model = learn_embeddings(walks, args, unique_words)
 	return model
-
-#nodes = np.load('/home/master/Desktop/GIT/node2vec-final/node2vec/src/nodes.npy')
-def word2id(word):
-	
-	for node in nodes:
-		if node[1]["label"] == word:
-			return str(node[0])
-	raise "word not in vocab"
-
-if __name__ == "__main__":
-	args = parse_args()
-	#model = main(args)
-	
-	#w1 = "you"
-	#w2 = "yourself"
-	#print(model.wv.similarity(word2id(w1), word2id(w2)))
-	#print(model.wv.similarity("1", "3"))
